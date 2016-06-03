06eede47f00dc6721930abf6a2cec12611a1a13e
bogdan8 <bogdanlitvin8@gmail.com>
2016-06-03 14:07:12 +0300

    add composer and add User

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,8 +3,8 @@
 Route::get('/', ['as' => 'index', 'uses' => 'IndexController@index']);
 Route::post('/', ['as' => 'index', 'uses' => 'IndexController@index']);
 
-/*
+
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
-]);*/
+]);
