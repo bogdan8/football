88d76096b2eed14469742b66df37f01a50bd3dde
bogdan8 <bogdanlitvin8@gmail.com>
2016-07-29 09:58:30 +0300

    done kfc fundraiser and kfc spending

--- resources/views/kfc/fundraiser.blade.php
+++ resources/views/kfc/fundraiser.blade.php
@@ -1,14 +1,53 @@
 @extends('layouts.app')
 @section('content')
+    <div class="body-header-block">
+        <div class="container">
+            <div class="row">
+                <b class="body-header-block-title-first">КФС</b>
+            </div>
+            <br>
+            <br>
 
-    <h4>Витрати</h4>
-    <pre>
-        <?php $i = 1; ?>
-        @foreach($kfc_fundraiser as $item)
-            №{{$i++}}
-            Товар або послуга: {!! $item->initials !!}
-            Дата: {!! $item->date !!}
-            Сума: {!! $item->sum !!}.00 грн
-        @endforeach
-    </pre>
+            <div class="row">
+                <b class="body-header-block-title-second">ЗБІР КОШТІВ</b>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <table class="table table-bordered table-spending">
+                <thead>
+                <tr style="width: 100%;">
+                    <th width="5%">№</th>
+                    <th width="75%">Прізвище Ім'я Побатькові</th>
+                    <th width="10%" style="text-align: center">Дата</th>
+                    <th width="10%" style="text-align: center">Сума</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                $i = 1;
+                $sum = 0;
+                ?>
+                @foreach($kfc_fundraiser as $item)
+                    <tr>
+                        <td>{{$i++}}</td>
+                        <td>{{ $item->goods_or_services }}</td>
+                        <td style="text-align: center">{{date_create($item->date)->Format('d.m.Y')}}</td>
+                        <td style="text-align: center">{{ $item->sum }}.00 грн</td>
+                        <?php $sum += $item->sum;?>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+            <table class="table-spending" style="margin-top: -20px">
+                <tr class="table-bottom">
+                    <td width="5%"></td>
+                    <td width="75%"></td>
+                    <td width="10%" style="text-align: center" class="table-spending-count-name">ВСЬОГО</td>
+                    <td width="10%" style="text-align: center" class="table-spending-count-value">{{$sum}}.00 грн</td>
+                </tr>
+            </table>
+        </div>
+    </div>
 @stop
--- resources/views/kfc/spending.blade.php
+++ resources/views/kfc/spending.blade.php
@@ -1,14 +1,60 @@
 @extends('layouts.app')
 @section('content')
+    <div class="body-header-block">
+        <div class="container">
+            <div class="row">
+                <b class="body-header-block-title-first">КФС</b>
+            </div>
+            <br>
+            <br>
 
-    <h4>Витрати</h4>
-    <pre>
-        <?php $i = 1; ?>
-        @foreach($kfc_spending as $item)
-            №{{$i++}}
-            Товар або послуга: {!! $item->goods_or_services !!}
-            Дата: {!! $item->date !!}
-            Сума: {!! $item->sum !!}.00 грн
-        @endforeach
-    </pre>
+            <div class="row">
+                <b class="body-header-block-title-second">ВИТАРТИ</b>
+            </div>
+            <br>
+            <br>
+
+            <div class="row">
+                <b class="body-header-block-title-three">СПИСОК КУПЛЕНИХ ТОВАРІВ ТА ОПЛАЧЕНИХ РОБІТ</b>
+            </div>
+            <br>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <table class="table table-bordered table-spending">
+                <thead>
+                <tr style="width: 100%;">
+                    <th width="5%">№</th>
+                    <th width="75%">Товар або послуга</th>
+                    <th width="10%" style="text-align: center">Дата</th>
+                    <th width="10%" style="text-align: center">Сума</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                $i = 1;
+                $sum = 0;
+                ?>
+                @foreach($kfc_spending as $item)
+                    <tr>
+                        <td>{{$i++}}</td>
+                        <td>{{ $item->goods_or_services }}</td>
+                        <td style="text-align: center">{{date_create($item->date)->Format('d.m.Y')}}</td>
+                        <td style="text-align: center">{{ $item->sum }}.00 грн</td>
+                        <?php $sum += $item->sum;?>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+            <table class="table-spending" style="margin-top: -20px">
+                <tr class="table-bottom">
+                    <td width="5%"></td>
+                    <td width="75%"></td>
+                    <td width="10%" style="text-align: center" class="table-spending-count-name">ВСЬОГО</td>
+                    <td width="10%" style="text-align: center" class="table-spending-count-value">{{$sum}}.00 грн</td>
+                </tr>
+            </table>
+        </div>
+    </div>
 @stop
