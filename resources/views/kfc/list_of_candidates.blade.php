720115c61a787aa3d1861bc1e11ae20c5a56e902
bogdan8 <bogdanlitvin8@gmail.com>
2016-07-29 10:48:47 +0300

    done kfc list_of_candidates

--- resources/views/kfc/list_of_candidates.blade.php
+++ resources/views/kfc/list_of_candidates.blade.php
@@ -1,15 +1,48 @@
 @extends('layouts.app')
 @section('content')
+    <div class="body-header-block">
+        <div class="container">
+            <div class="row">
+                <b class="body-header-block-title-first">КФС</b>
+            </div>
+            <br>
+            <br>
 
-    <h4>Витрати</h4>
-    <pre>
-        <?php $i = 1; ?>
-        @foreach($candidates_year as $item)
-            Роки: <a href="/kfc/list_of_candidates/{!! $item->years !!}">{!! $item->years !!}</a>
-            @foreach($item->kfc_list_of_candidates_people as $list_people)
-                №{{$i}}
-                Прізвище імя: {{$list_people->initials}}
-            @endforeach
-        @endforeach
-    </pre>
+            <div class="row">
+                <b class="body-header-block-title-second">СПИСОК ПРЕТЕНДЕНТІВ</b>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7">
+                <div class="list-candidates-block">
+                    <div class="list-candidates-block-people">
+                        <?php $i = 1; ?>
+                        @foreach($candidates_year[0]->kfc_list_of_candidates_people as $list_people)
+                            <p class="list-candidates-block-people-name">{{$i++}}
+                                &#160;&#160;&#160;&#160;&#160;{{$list_people->initials}}</p>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-5">
+                <div class="list-candidates-block-years">
+                    <div class="list-candidates-block-year-active">
+                        <div class="list-candidates-block-year-active-corner"></div>
+                        <a class="list-candidates-block-year-name-active"
+                           href="/kfc/list_of_candidates/{{ $candidates_year[0]->years }}">{{ $candidates_year[0]->years }}
+                            РОКИ</a>
+                    </div>
+                    <?php for ($j = 1; $j < count($candidates_year); $j++) { ?>
+                    <div class="list-candidates-block-year">
+                        <a class="list-candidates-block-year-name"
+                           href="/kfc/list_of_candidates/{{ $candidates_year[$j]->years }}">{{ $candidates_year[$j]->years }}
+                            РОКИ</a>
+                    </div>
+                    <?php } ?>
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
--- resources/views/kfc/list_of_candidates_year.blade.php
+++ resources/views/kfc/list_of_candidates_year.blade.php
@@ -1,16 +1,51 @@
 @extends('layouts.app')
 @section('content')
+    <div class="body-header-block">
+        <div class="container">
+            <div class="row">
+                <b class="body-header-block-title-first">КФС</b>
+            </div>
+            <br>
+            <br>
 
-    <h4>Витрати</h4>
-    @foreach($candidates_year_all as $item)
-        Роки: <a href="/kfc/list_of_candidates/{!! $item->years !!}">{!! $item->years !!}</a>
-    @endforeach
-    <pre>
-        <?php $i = 1; ?>
-            Роки: <a href="/kfc/list_of_candidates/{!! $candidates_year->years !!}">{!! $candidates_year->years !!}</a>
-            @foreach($candidates_year->kfc_list_of_candidates_people as $list_people)
-                №{{$i}}
-                Прізвище імя: {{$list_people->initials}}
-            @endforeach
-    </pre>
+            <div class="row">
+                <b class="body-header-block-title-second">СПИСОК ПРЕТЕНДЕНТІВ</b>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7">
+                <div class="list-candidates-block">
+                    <div class="list-candidates-block-people">
+                        <?php $i = 1; ?>
+                        @foreach($candidates_year->kfc_list_of_candidates_people as $list_people)
+                            <p class="list-candidates-block-people-name">{{$i++}}
+                                &#160;&#160;&#160;&#160;&#160;{{$list_people->initials}}</p>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-5">
+                <div class="list-candidates-block-years">
+                    @foreach($candidates_year_all as $item)
+                        @if($item->years == $candidates_year->years)
+                            <div class="list-candidates-block-year-active">
+                                <div class="list-candidates-block-year-active-corner"></div>
+                                <a class="list-candidates-block-year-name-active"
+                                   href="/kfc/list_of_candidates/{{ $item->years }}">{{ $item->years }}
+                                    РОКИ</a>
+                            </div>
+                        @else
+                            <div class="list-candidates-block-year">
+                                <a class="list-candidates-block-year-name"
+                                   href="/kfc/list_of_candidates/{{ $item->years }}">{{ $item->years }}
+                                    РОКИ</a>
+                            </div>
+                        @endif
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
